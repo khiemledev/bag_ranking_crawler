e147ca27e5422d4912688c72415f8c0d445b1f31
Khiem Le <theinnocentman1@gmail.com>
2023-05-02 17:04:06 +0700

    Use getenv instead of hardcode

--- bag_ranking_crawler/pipelines.py
+++ bag_ranking_crawler/pipelines.py
@@ -6,7 +6,7 @@
 
 import json
 import time
-from email import header
+from os import getenv
 
 import pika
 import pymongo
@@ -16,11 +16,14 @@ import pymongo
 
 class BagPipeline:
     def __init__(self):
-        self.client = pymongo.MongoClient(
-            "mongodb://admin:Embery#1234@51.161.130.170:27017")
-        database_name = 'kl_bag_ranking'
-        self.db = self.client[database_name]
-        self.collection = self.db['bag_raw']
+        # Load env variables
+        self.mongo_host = getenv('MONGO_CONNECTION_STRING')
+        self.database_name = getenv('MONGO_DBNAME', 'kl_bag_ranking')
+        self.collection_name = getenv('MONGO_COLLECTION', 'bag_raw')
+
+        self.client = pymongo.MongoClient(self.mongo_host)
+        self.db = self.client[self.database_name]
+        self.collection = self.db[self.collection_name]
         self.collection.create_index('link', unique=True)
 
     def process_item(self, item, spider):
@@ -50,19 +53,26 @@ class BagPipeline:
 
 class CrawlingLinkPipeline:
     def __init__(self):
+        self.rabbitmq_host = getenv('RABBITMQ_HOST', 'localhost')
+        self.rabbitmq_username = getenv('RABBITMQ_USERNAME')
+        self.rabbitmq_password = getenv('RABBITMQ_PASSWORD')
+
         credentials = pika.PlainCredentials(
-            'admin',
-            'emberyembery',
+            self.rabbitmq_username,
+            self.rabbitmq_password,
         )
-        self.rbmq_queue = 'bag_ranking_crawl_link'
+        self.rabbitmq_exchange = getenv(
+            'RABBITMQ_EXCHANGE', 'bag_ranking_crawl_link')
+        self.rbmq_queue = getenv('RABBITMQ_QUEUE', 'bag_ranking_crawl_link')
         self.rbmq_connection = pika.BlockingConnection(
             pika.ConnectionParameters(
-            host='rabbitmq.embery.com.au',
-            credentials=credentials,
-        ))
+                host=self.rabbitmq_host,
+                # credentials=credentials if self.rabbitmq_username else None,
+            ),
+        )
         self.rbmq_channel = self.rbmq_connection.channel()
         self.rbmq_channel.exchange_declare(
-            exchange='bag_ranking_crawl_link',
+            exchange=self.rabbitmq_exchange,
             exchange_type='x-message-deduplication',
             arguments=dict({'x-cache-size': '5'}),
         )
@@ -73,9 +83,9 @@ class CrawlingLinkPipeline:
         self.rbmq_channel.queue_declare(
             queue=queue_name,
         )
-        self.rbmq_channel.queue_bind(queue_name, 'bag_ranking_crawl_link')
+        self.rbmq_channel.queue_bind(queue_name, self.rabbitmq_exchange)
         self.rbmq_channel.basic_publish(
-            exchange='bag_ranking_crawl_link',
+            exchange=self.rabbitmq_exchange,
             routing_key=routing_key,
             body=json.dumps(dict(item)),
             properties=pika.BasicProperties(
